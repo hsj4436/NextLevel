a63676c8c2454cffdd25c9ca1be0c05688ee7b7a
alsuddlek1 <minyoung99311@gmail.com>
2023-11-08 01:12:39 +0900

    :sparkles: 발음평가 구현
    발음평가 구현

--- frontend/src/hooks/drama/useDramaArtistClipHook.tsx
+++ frontend/src/hooks/drama/useDramaArtistClipHook.tsx
@@ -5,12 +5,13 @@ export const useDramaArtistCliptGetHook = () => {
   const [dramaAritstClip, setDramaAritstClip] = useState<any>();
   const getDramaAritstClip = async (id: any) => {
     const res = await DramaArtistClipGet(id);
-    setDramaAritstClip(res.data.data.clips);
+    console.log(res);
+    // setDramaAritstClip(res.data.data.problems);
   };
   const [dramaSelectArtistClip, setDramaSelectArtistClip] = useState<any>();
   const getDramaSelectAritstClip = async (id: any) => {
     const res = await DramaArtistClipGet(id);
-    setDramaSelectArtistClip(res.data.data.clips);
+    // setDramaSelectArtistClip(res.data.data.problems);
   };
   return {
     dramaAritstClip,
--- frontend/src/pages/learning/learntest/index.tsx
+++ frontend/src/pages/learning/learntest/index.tsx
@@ -74,43 +74,6 @@ const LearnTest = () => {
     }
   }, [audioUrl]);
 
-  // const sendToserverFun = () => {
-  //   const formData = new FormData();
-
-  //   const sendToServer = new File([audioUrl], "test.wav", {
-  //     type: "audio/wav",
-  //   });
-
-  //   console.log("111", sendToServer);
-
-  //   // // 파일 확인용 다운로드 함수
-  //   function download() {
-  //     const a = document.createElement("a");
-  //     const url = URL.createObjectURL(sendToServer);
-
-  //     a.href = url;
-  //     a.download = sendToServer.name;
-
-  //     document.body.appendChild(a);
-  //     a.click();
-  //     document.body.removeChild(a);
-  //   }
-
-  //   try {
-  //     // 확인용 파일 다운로드
-  //     download();
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-
-  //   const text =
-  //     "안녕하세요 저는 스물다섯살 손민영입니다 벌써 십이월이에요 겨울이네요";
-
-  //   formData.append("script", text);
-  //   formData.append("wavFile", sendToServer);
-  //   getScenarioProbelem(formData);
-  // };
-
   const sendToserverFun = useCallback(async () => {
     if (audioUrl) {
       setSound(URL.createObjectURL(audioUrl));
@@ -120,28 +83,13 @@ const LearnTest = () => {
       const wav = audioBufferToWav(audioBuffer);
       const wavFile = new File([wav], "sound", { type: "audio/wav" });
 
-      const script = {
-        ko: "안녕하세요",
-        video_sentence_id: 3,
-      };
+      const script = "안녕하세요";
 
       const formdata = new FormData();
-      // formdata.enctype = "multipart/form-data";
-      formdata.append("sound", wavFile);
-      formdata.append(
-        "script",
-        new Blob([JSON.stringify(script)], { type: "application/json" })
-      );
+      formdata.append("wavFile", wavFile);
+      formdata.append("script", script);
 
-      fetch(`http://k9e204.p.ssafy.io/api/evaluate/result`, {
-        method: "POST",
-        body: formdata,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("data", data);
-          // setScoreData(data);
-        });
+      getScenarioProbelem(formdata);
     } else {
       console.log("no audio");
     }
